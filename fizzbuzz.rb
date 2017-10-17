a124e5bb894488fb0c19a9492dfc840ddd32ae9f
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-17 18:36:28 +0100

    added comments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,12 @@
 class Fizzbuzz
-  def run(num1,num2)
-    fizz = 3
-    buzz = 5
-    array = []
+  def run(num1,num2) #Run function, creates array of values, able to set range here.
+    array = []        #Create empty array.
     for i in num1..num2
-      if i % fizz == 0 && i % buzz == 0
+      if i % 3 == 0 && i % 5 == 0
         array.push("FizzBuzz")
-      elsif i % fizz == 0
+      elsif i % 3 == 0
         array.push("Fizz")
-      elsif i % buzz == 0
+      elsif i % 5 == 0
         array.push("Buzz")
       else
         array.push(i)
